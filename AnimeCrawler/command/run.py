253fe67057b1a638afce6f412f5665876a6dd34d
Senvlin_ <2994515402@qq.com>
2023-04-22 21:21:16 +0800

    重构,代码解耦

--- AnimeCrawler/command/run.py
+++ AnimeCrawler/command/run.py
@@ -1,13 +1,18 @@
 import argparse
 import re
+import sys
+from collections import namedtuple
 
+from AnimeCrawler.log import get_logger
 from AnimeCrawler.mhyyy.spider import AnimeSpider
 
 
 class CrawlerCommands:
     '''https://docs.python.org/zh-cn/3/library/argparse.html#quick-links-for-add-argument'''
 
-    def is_url(self, string):
+    name = 'Commands'
+
+    def is_url(self, string: str):
         '''解析是否为url
 
         Args:
@@ -35,6 +40,7 @@ class CrawlerCommands:
             description='* AnimeCrawler v0.1.1 - 一个可免费下载动漫的爬虫\n* Repo: https://github.com/Senvlin/AnimeCrawler',
             epilog='Had Issues? Go To -> https://github.com/Senvlin/AnimeCrawler/issues',
             add_help=False,  # 在这里是为了自定义help文档所以禁用了
+            exit_on_error=False,
         )
         group1 = parser.add_argument_group('Commands', '主要命令')
         group1.add_argument('download', help="下载动漫", nargs='?', default=False)
@@ -52,32 +58,28 @@ class CrawlerCommands:
         group3.add_argument("-h", dest='help', help="获取帮助，并退出程序", action='store_true')
         return parser
 
+    def catch_errors(self, parse: argparse.Namespace):
+        Errors = namedtuple("Errors", ('error_code', 'error_reason', 'output'))
+        if parse.download != 'download' and parse.search != 'search':
+            return Errors('400', 'null_command', f'无效的命令 {parse.download}')
+        elif parse.search == 'search':
+            return Errors('401', 'not_supported_command', f'未支持的命令 {parse.search}')
+        elif not parse.title:
+            return Errors('402', 'null_title', f'标题 {parse.title} 为空')
+        elif not self.is_url(parse.url or ''):
+            return Errors('403', 'is_not_url', f'{parse.url} 不为合法的url')
+
     def parse(self, test_list: list = None):
         parser = self.get_parser()
+        logger = get_logger(self.name)
         parse = parser.parse_args(test_list)
-        if parse.help:
+        if parse.help or not parse.download:
             parser.print_help()
-            parser.exit()
-        if parse.download != 'download' and parse.search != 'search':
-            if parse.is_test:
-                return 'null_command'
-            raise ValueError(f'无效的命令 {parse.download}')
-        elif parse.search == 'search':
-            if parse.is_test:
-                return 'have not supported \'search\' command'
-            raise NotImplementedError(f'未支持的命令 {parse.search}')
-        elif parse.title is None:
-            if parse.is_test:
-                return 'null_title'
-            raise ValueError(f'{parse.title} 为空')
-        elif not self.is_url(parse.url):
-            if parse.is_test:
-                return 'is_not_url'
-            raise ValueError(f'{parse.url} 不为合法的url')
+        if error := self.catch_errors(parse):
+            logger.error(error.output)
         else:
-            if parse.is_test:
-                return 'OK'
             AnimeSpider.init(parse.title, parse.url, parse.can_del_ts).start()
+        sys.exit()
 
 
 def main():
