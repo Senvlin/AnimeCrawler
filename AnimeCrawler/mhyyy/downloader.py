0c6181b7113d4d05dd0237b204abfcbcde90c3fd
Senvlin_ <2994515402@qq.com>
2023-05-02 23:12:24 +0800

    啊啊啊

--- AnimeCrawler/mhyyy/downloader.py
+++ AnimeCrawler/mhyyy/downloader.py
@@ -16,7 +16,7 @@ class Downloader:
     def current_sesion(self):
         if not self.session:
             self.session = aiohttp.ClientSession(
-                connector=aiohttp.TCPConnector(limit=40, verify_ssl=False)
+                connector=aiohttp.TCPConnector(limit=50, verify_ssl=False)
             )
         return self.session
 
@@ -26,21 +26,24 @@ class Downloader:
         url: str,
         error_times: int = 1,
     ):
-        resp = await self.current_sesion.get(
-            url=url,
-            headers={'User-Agent': 'Mozilla/5.0', 'Transfer-Encoding': 'chunked'},
-        )
         try:
-            text = await resp.content.read()
-            return (text, title)
+            async with self.semaphore:
+                resp = await self.current_session.get(
+                    url=url,
+                    headers={
+                        'User-Agent': 'Mozilla/5.0',
+                        ' Transfer-Encoding': 'chunked',
+                    },
+                )
+                text = await resp.content.read()
+                await write(path, text, title, suffix='ts', mode='wb')
         except aiohttp.ClientPayloadError as e:  # 报错时重新下载
             if error_times == 3:
-                raise Warning(f'下载{title}.ts时发生错误') from e
-            print(f'下载{title}.ts时发生错误，正在重试第{error_times}次')
-            return await self.get_ts_file(title, url, error_times + 1)
+                raise ValueError(f'无法下载{title}.ts') from e
+            self.logger.warning(f'下载{title}.ts时发生错误，正在重试第{error_times}次')
+            return await self.get_ts_file(title, url, path, error_times + 1)
         except Exception as e:
-            print(f'下载{title}.ts时发生{e}')
-            await asyncio.sleep(0)
+            raise ValueError(f'下载{title}.ts时，{e}') from e
         finally:
             resp.close()
 
@@ -53,10 +56,5 @@ class Downloader:
             self.get_ts_file(str(index).zfill(4), url)
             for index, url in enumerate(self.urls)
         ]
-        for task in tqdm.asyncio.tqdm.as_completed(tasks, desc=f"正在下载第{episodes}集视频"):
-            result = await task
-            text_1, title = (
-                (b'error', 'error') if result is None else result
-            )  # result为空时返回'error'
-            await write(path, text_1, title, suffix='ts', mode='wb')
+        await tqdm.asyncio.tqdm.gather(*tasks, desc=f"正在下载第{episodes}集视频")
         await self.close_session()
