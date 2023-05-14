e2e9bb6244e7d6261c91769f02e6c47b0ab331bb
Senvlin_ <2994515402@qq.com>
2023-05-14 13:57:22 +0800

    扩展get_logger 将其放到 logger类中

--- AnimeCrawler/log.py
+++ AnimeCrawler/log.py
@@ -8,11 +8,24 @@ _str_to_level = {
 }
 
 
-def get_logger(name='', level='DEBUG') -> logging.Logger:
-    logging.basicConfig(
-        level=_str_to_level[level],
-        format='[\033[0;36;40m%(asctime)s.%(msecs)03d\033[0m] %(name)-7s | %(levelname)-7s | %(filename)s - %(lineno)d: %(message)s',
-        datefmt='%Y-%d-%m %I:%M:%S',
-    )
-    logger = logging.getLogger(name)
-    return logger
+class Logger:
+    _level = 'ERROR'
+
+    @property
+    def level(self, level):
+        return level
+
+    @level.setter
+    def level(self, level):
+        if _str_to_level.get(level, None):
+            print(level)
+            self._level = level
+
+    def get_logger(self, name='') -> logging.Logger:
+        logging.basicConfig(
+            level=self._level,
+            format='[\033[0;36;40m%(asctime)s.%(msecs)03d\033[0m] %(name)-7s | %(levelname)-7s | %(filename)s - %(lineno)d: %(message)s',
+            datefmt='%Y-%d-%m %I:%M:%S',
+        )
+        logger = logging.getLogger(name)
+        return logger
