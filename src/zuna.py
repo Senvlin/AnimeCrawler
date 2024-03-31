acf8243a53aad7c6414ebfd41657dd7e27301078
Senvlin_ <2994515402@qq.com>
2024-03-31 13:54:24 +0800

    ✨ feat(Parser): 增加了m3u8解析器，完成对每一集的封装
    将Parser类提取出来变为基类，M3u8parser与EpisodeParser继承于此。
    新增Episode类，完成了对每一集的封装，下一步可以进行全篇下载。
    📃 docs(zuna.py)：
    
    完善了大部分函数/类的注释，应该比之前容易理解多了

--- src/zuna.py
+++ src/zuna.py
@@ -2,12 +2,13 @@ import asyncio
 import ctypes.wintypes
 import pathlib
 import re
-from typing import AsyncGenerator, Generator, Iterable, Optional
+from dataclasses import Field, InitVar, dataclass, field
+from typing import AsyncGenerator, Generator, Optional
 from urllib.parse import urljoin
 
 import aiofiles
 import aiohttp
-from bs4 import BeautifulSoup, NavigableString, Tag
+import lxml.html
 
 
 def get_video_path():
@@ -21,14 +22,58 @@ VIDEO_FOLDER_PATH = pathlib.Path(get_video_path())
 ANIME_NAME = "三体"
 
 
-class Anime: ...
+class Parser:
+    def __init__(self, html_str) -> None:
+        self.root = lxml.html.fromstring(html_str)
+
+
+class M3u8Parser(Parser):
+    def __init__(self, html_str) -> None:
+        super(__class__, self).__init__(html_str)
+
+    @property
+    def m3u8_url(self) -> str:
+        player_datas = self.root.xpath(
+            '//div[@class="player-box-main"]/script[1]/text()'
+        )
+        _m3u8_url = re.findall(r'"url":"(.*?)",', player_datas[0])[0]
+        return _m3u8_url
+
+
+class EpisodeParser(Parser):
+    """对每一集信息的解析"""
+
+    # DONE 完成对每一集信息的解析
+    # DONE 应先获得每一集信息，再从信息中过滤出url和name
+
+    def __init__(self, html_str) -> None:
+        super(__class__, self).__init__(html_str)
+
+    @property
+    def episode_infos(self) -> Generator:
+        infos = self.root.xpath(
+            '//div[@class="module-list sort-list tab-list play-tab-list active"]/div/div/a'
+        )
+        yield from infos
+
+    @property
+    def episode_url_parts(self):
+        for info in self.episode_infos:
+            yield info.attrib["href"]
+
+    @property
+    def episode_names(self):
+        for info in self.episode_infos:
+            yield info.xpath("span/text()")
 
 
 class M3u8:
+    """对m3u8文件的包装"""
+
     def __init__(
         self,
         response: aiohttp.ClientResponse,
-        file_path: Optional[str] = "ceshi.m3u8",
+        file_path: Optional[str | pathlib.Path] = "ceshi.m3u8",
     ):
         self.response = response
         self.url = str(response.url)
@@ -52,8 +97,13 @@ class M3u8:
                 url = urljoin(self.url, ts_url_part)
                 yield url
 
+    def __repr__(self) -> str:
+        return f"<M3u8File path={self.file_path}>"
+
 
 class Ts:
+    """对ts文件的包装"""
+
     def __init__(self, response: aiohttp.ClientResponse):
         self.response = response
         self.name = response.url.parts[-1]
@@ -70,9 +120,25 @@ class Ts:
         return f"<TsFile Name={self.name}>"
 
 
+@dataclass
+class Episode:
+    name: str
+    root_url: InitVar[str]
+    episode_url_part: InitVar[str]
+    episode_url: Field[str] = field(init=False)
+    m3u8_url: Optional[str] = None
+    m3u8_path: Optional[str | pathlib.Path] = None
+
+    def __post_init__(self, root_url, episode_url_part: str):
+        self.episode_url = urljoin(root_url, episode_url_part)
+
+
 class Spider:
+    """主要爬取一集的m3u8文件和ts文件，并把ts文件合并为mp4文件"""
+
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
             Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"
     }
 
@@ -83,11 +149,17 @@ class Spider:
 
     @property
     def request_session(self) -> aiohttp.ClientSession:
+        """将session包装为单例模式，防止性能开销大
+
+        Returns:
+            aiohttp.ClientSession: 当时的session
+        """
         if self.session is None:
             self.session = aiohttp.ClientSession()
         return self.session
 
     async def crawler(self):
+        """相当于Worker，不断爬取文件，直到队列为空"""
         while not self.ts_url_queue.empty():
             url = await self.ts_url_queue.get()
             print(f"Worker is processing URL: {url}")
@@ -98,13 +170,26 @@ class Spider:
     #     await asyncio.sleep(2)
     #     self.ts_url_queue.task_done()
 
-    async def ts_crawl(self, url):
+    async def ts_crawl(self, url: str):
+        """具体的爬取任务
+
+        Args:
+            url (str): 要爬取的url
+        """
         async with self.request_session.get(url, headers=self.headers) as resp:
             ts_file = Ts(resp)
             self.ts_url_queue.task_done()
             await ts_file.save()
 
     async def m3u8_fetch(self, url) -> M3u8:
+        """爬取一集的m3u8文件，为之后的ts文件解析做铺垫
+
+        Args:
+            url (str): 一集的url
+
+        Returns:
+            M3u8
+        """
         async with self.request_session.get(url, headers=self.headers) as resp:
             r = await resp.text()
             m3u8_url_part = re.findall(r".*?.m3u8", r)[-1]
@@ -131,7 +216,8 @@ class Spider:
         await self.ts_url_queue.join()
         await self.request_session.close()
 
-    async def merge_ts(self):
+    async def merge_ts_files(self):
+        """合并ts文件为mp4文件， 目录在 VIDEO_FOLDER_PATH / ANIME_NAME"""
         cwd = VIDEO_FOLDER_PATH / ANIME_NAME
         print(cwd)
         ts_file_paths = (path for path in cwd.iterdir() if path.suffix == ".ts")
@@ -142,40 +228,14 @@ class Spider:
                     await parent_fp.write(text)
 
 
-class EpisodeParser:
-    # DONE 完成对每一集信息的解析
-    # DONE 应先获得每一集信息，再从信息中过滤出url和name
-
-    def __init__(self, html_str) -> None:
-        self.root = BeautifulSoup(html_str, 'html.parser')
-
-    @property
-    def episode_infos(self) -> Generator:
-        play_tab_list = self.root.find('div', class_='module-list sort-list tab-list play-tab-list active')
-        infos = play_tab_list.find_all('a', class_='module-play-list-link')
-        for info in infos:
-            yield info
-
-    @property
-    def episode_url_parts(self):
-        for info in self.episode_infos:
-            yield info.attrs['href']
-
-    @property
-    def episode_names(self):
-        for info in self.episode_infos:
-            yield info.text.replace('\n', '')
-
-
 async def main(url):
     # spider = Spider()
     # await spider.run(url)
-    # await spider.merge_ts()
+    # await spider.merge_ts_files()
     async with aiofiles.open("a.html", "r", encoding="utf-8") as fp:
         html_str = await fp.read()
-    parser = EpisodeParser(html_str)
-    for url in parser.episode_names:
-        print(url)
+    parser = M3u8Parser(html_str)
+    print(parser.m3u8_url)
 
 
 if __name__ == "__main__":
