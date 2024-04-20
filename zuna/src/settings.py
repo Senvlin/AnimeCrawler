52ee7bd956ea2de0276424b499ff5866078165d9
Senvlin_ <2994515402@qq.com>
2024-04-20 22:28:06 +0800

    🌈 style(settings): 略微调整格式

--- zuna/src/settings.py
+++ zuna/src/settings.py
@@ -1,6 +1,6 @@
-
 # TODO 以后改成从配置文件中读取
-ANIME_NAME = "鬼灭之刃"
-MAX_CONCURRENT_REQUESTS = (
-    16  # 最大并发请求数 创建的worker不超过此数量 建议 <= 16
-)
+LOG_LEVEL = "DEBUG"
+ANIME_NAME = "test"
+# 最大并发请求数 创建的worker不超过此数量 建议 <= 16
+MAX_CONCURRENT_REQUESTS = 16  
+
