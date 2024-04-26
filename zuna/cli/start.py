5fe0384b1b8d7a7fb836c3c1f253d6e970f94c5c
Senvlin_ <2994515402@qq.com>
2024-04-26 21:10:12 +0800

    ✨ feat(cli): 改变文件入口，初步实现cli工具
    将文件入口由main变为zuna/cli/start(以后还会改名)，通过 py zuna/cli/start.py COMMAND [--ARGS]实现调用，子命令有config download 详情请看-help

---
+++ zuna/cli/start.py
@@ -0,0 +1,56 @@
+import asyncio
+import sys
+
+import click
+
+from zuna.src.config import Config
+from zuna.src.engine import Engine
+
+cfg = Config()
+
+
+@click.group()
+@click.version_option(
+    version="0.1.0",
+    prog_name="Zuna",
+    message="%(prog)s [v%(version)s] - A simple anime downloader",
+)
+def cli(): ...
+
+
+@cli.command(help="Download anime from the given url")
+@click.option(
+    "-u",
+    "--root_url",
+    help="The anime url to start scraping from",
+    required=True,
+)
+@click.option("-n", "--anime_name", help="The name of the anime", required=True)
+def download(root_url, anime_name):
+    cfg.config["common"]["anime_name"] = anime_name
+    asyncio.run(engine.run(root_url))
+
+
+@cli.command(help="Configure the program")
+@click.option(
+    "--log_level", type=str, help="The log level to use", default="INFO"
+)
+@click.option(
+    "--max_concurrent_requests",
+    type=click.IntRange(1, 32),
+    help="The maximum number of concurrent requests",
+    default=16,
+)
+def config(log_level, max_concurrent_requests):
+    if log_level not in ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"):
+        raise click.BadParameter("Invalid log level")
+    cfg.config["common"]["log_level"] = log_level
+    cfg.config["download"]["max_concurrent_requests"] = str(
+        max_concurrent_requests
+    )
+
+
+engine = Engine()
+
+if __name__ == "__main__":
+    cli()
