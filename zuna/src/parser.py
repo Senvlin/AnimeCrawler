f2bcd5cb09f205074255fe59f3d4a5cd5b9abf3b
Senvlin_ <2994515402@qq.com>
2024-04-29 22:44:49 +0800

    🦄 refactor(M3u8Parser): 拆分结构，设置parse接口
    parse() 可在其中编写xpath，css等解析m3u8_url，必须返回字符串，可使用self._root属性获取html

--- zuna/src/parser.py
+++ zuna/src/parser.py
@@ -3,20 +3,20 @@ from typing import Generator
 
 import lxml.html
 
-# TODO 用@dataclasses重构代码
 class M3u8Parser:
     def __init__(self, html_str) -> None:
         self._root = lxml.html.fromstring(html_str)
 
     @property
     def m3u8_url(self) -> str:
+        return self.parse()
+
+    def parse(self):
         player_datas = self._root.xpath(
             '//div[@class="player-box-main"]/script[1]/text()'
         )
-        _m3u8_url: str = re.findall(r'"url":"(.*?)",', player_datas[0])[0]
-        _m3u8_url = _m3u8_url.replace("\\", "")
+        _m3u8_url: str = re.findall(r'"url":"(.*?)",', player_datas[0])[0].replace("\\", "")
         return _m3u8_url
-
 
 class EpisodesParser:
     """对每一集信息的解析"""
@@ -24,14 +24,25 @@ class EpisodesParser:
     def __init__(self, html_str) -> None:
         self._root = lxml.html.fromstring(html_str)
 
-    @property
-    def episode_infos(self) -> Generator:
+    def parse(self):
+        """
+        对外的接口，解析每一集的信息
+
+        Returns:
+            Iterable: [Required] 一集的信息
+        """
+        # HACK episode_url_parts episode_names 都依赖于此 想要把解析逻辑分离还得再改改
+        
         infos = self._root.xpath(
             '//div \
             [@class="module-list sort-list tab-list play-tab-list active"] \
             /div/div/a'
         )
-        yield from infos
+        return infos
+
+    @property
+    def episode_infos(self) -> Generator:
+        yield from self.parse()
 
     @property
     def episode_url_parts(self):
